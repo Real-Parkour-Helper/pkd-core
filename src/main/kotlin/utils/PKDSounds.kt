2488c9e265b0b6536af06dc497d3c2770d532fc5
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2025-06-14 20:51:43 +0200

    pkdsounds util object

---
+++ src/main/kotlin/utils/PKDSounds.kt
@@ -0,0 +1,22 @@
+package utils
+
+import org.bukkit.Sound
+import org.bukkit.entity.Player
+
+object PKDSounds {
+
+    /**
+     * Plays the sound for a player when they reach a checkpoint.
+     */
+    fun playCheckpointSound(player: Player) {
+        player.playSound(player.location, Sound.LEVEL_UP, 1.0f, 1.0f)
+    }
+
+    /**
+     * Plays the sound for a player when they reset to the last checkpoint.
+     */
+    fun playResetSound(player: Player) {
+        player.playSound(player.location, Sound.ENDERMAN_TELEPORT, 1.0f, 1.0f)
+    }
+
+}
