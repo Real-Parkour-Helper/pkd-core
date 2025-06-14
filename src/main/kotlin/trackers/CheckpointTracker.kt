d1a01e1cb75c2a299e71fda92f1f6a8c0983b0f9
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2025-06-14 20:38:19 +0200

    checkpoint tracker

---
+++ src/main/kotlin/trackers/CheckpointTracker.kt
@@ -0,0 +1,47 @@
+package trackers
+
+import org.bukkit.Location
+import org.bukkit.entity.Player
+
+class CheckpointTracker(
+    private val checkpointLocations: List<Location>,
+    private val players: List<Player>,
+    private val onCheckpoint: (Player, Int) -> Unit
+) {
+
+    private val checkpointMap: MutableMap<Player, Int> = mutableMapOf()
+
+    init {
+        for (player in players) {
+            checkpointMap[player] = 0 // Initialize all players at checkpoint 0
+        }
+    }
+
+    /**
+     * Run the tick - check for checkpoint updates. This should be run every two ticks!
+     */
+    fun tick() {
+        for (player in players) {
+            val location = player.location
+            val currentCheckpoint = checkpointMap[player] ?: throw IllegalStateException("Player not found in checkpoint map")
+            val nextCheckpoint = checkpointLocations.getOrNull(currentCheckpoint + 1) ?: continue // No next checkpoint, so the player is already done
+
+            if (location.distance(nextCheckpoint) < 2.5) {
+                checkpointMap[player] = currentCheckpoint + 1
+                onCheckpoint(player, currentCheckpoint + 1) // do something with this
+            }
+        }
+    }
+
+    /**
+     * Resets the player to their last checkpoint.
+     */
+    fun resetToCheckpoint(player: Player) {
+        val currentCheckpoint = checkpointMap[player] ?: throw IllegalStateException("Player not found in checkpoint map")
+        val location = checkpointLocations[currentCheckpoint].clone().add(0.5, 0.5, 0.5)
+        player.teleport(location)
+    }
+
+    fun getCheckpoint(p: Player): Int? = checkpointMap[p]
+    fun hasFinished(p: Player)    = (checkpointMap[p] ?: -1) >= checkpointLocations.lastIndex
+}
