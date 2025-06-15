8be16a9f55a972ca1a0a30628c359f7f35f839b8
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2025-06-15 23:01:55 +0200

    clear undefined slots in custom layouts

--- src/main/kotlin/org/realparkourhelper/core/hotbar/HotbarLayout.kt
+++ src/main/kotlin/org/realparkourhelper/core/hotbar/HotbarLayout.kt
@@ -4,9 +4,13 @@ import org.bukkit.entity.Player
 
 class HotbarLayout(private val slots: Map<Int, HotbarSlot>) {
     fun applyTo(player: Player) {
-        slots.forEach { (index, slot) ->
-            player.inventory.setItem(index, slot.item)
+        val inv = player.inventory
+
+        for (i in 0..8) {
+            val slot = slots[i]
+            inv.setItem(i, slot?.item)
         }
+
         HotbarManager.setLayout(player, this)
     }
 
