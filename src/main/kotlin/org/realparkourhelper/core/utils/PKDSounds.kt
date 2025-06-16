e17c157b1467dfb80a117fde438ed2c4ec94e609
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2025-06-16 14:31:21 +0200

    more sound effects

--- src/main/kotlin/org/realparkourhelper/core/utils/PKDSounds.kt
+++ src/main/kotlin/org/realparkourhelper/core/utils/PKDSounds.kt
@@ -9,14 +9,34 @@ object PKDSounds {
      * Plays the sound for a player when they reach a checkpoint.
      */
     fun playCheckpointSound(player: Player) {
-        player.playSound(player.location, Sound.LEVEL_UP, 1.0f, 1.0f)
+        player.playSound(player.location, Sound.LEVEL_UP, 1f, 1f)
     }
 
     /**
      * Plays the sound for a player when they reset to the last checkpoint.
      */
     fun playResetSound(player: Player) {
-        player.playSound(player.location, Sound.ENDERMAN_TELEPORT, 1.0f, 1.0f)
+        player.playSound(player.location, Sound.ENDERMAN_TELEPORT, 8f, 4.05f)
     }
 
+    /**
+     * Plays the sound for a player when they toggle visibility.
+     */
+    fun playToggleVisibilitySound(player: Player) {
+        player.playSound(player.location, Sound.CLICK, 0.55f, 2f)
+    }
+
+    /**
+     * Plays the sound for a player when they are ready to use a boost.
+     */
+    fun playBoostReadySound(player: Player) {
+        player.playSound(player.location, Sound.CHICKEN_EGG_POP, 1f, 1f)
+    }
+
+    /**
+     * Plays the sound for a player when the countdown updates.
+     */
+    fun playTimerCountdownSound(player: Player) {
+        player.playSound(player.location, Sound.NOTE_STICKS, 20f, 1f)
+    }
 }
